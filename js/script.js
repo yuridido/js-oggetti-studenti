759e14d82c2cd60751e918d6a15ac928a5efc81c
yuridido <yuridido@yahoo.it>
2020-08-26 18:33:20 +0200

    terzo step

--- js/script.js
+++ js/script.js
@@ -8,47 +8,73 @@
 // Usate prima i console.log e poi provare a stampare con jQuery:
 // BONUS: Provate ad utilizzare Handlebars.
 
-// creo l'oggetto studente
-var studente = {
-    nome : 'tizio',
-    cognome : 'rossi',
-    eta : '23'
-};
-
-// lo scrivo nel dom
-var datiStudente = '';
-for (var key in studente) {
-    datiStudente += (" " + studente[key]);
-};
-
-$('#stud').append(datiStudente);
-
-// creo array di oggetti
-
-var elencoStudenti = [
-    {
-        nome : 'caio',
-        cognome : 'bianchi',
+$(document).ready(function(){
+    // creo l'oggetto studente
+    var studente = {
+        nome : 'tizio',
+        cognome : 'rossi',
         eta : '23'
-    },
-    {
-        nome : 'sempronio',
-        cognome : 'verdi',
-        eta : '25'
-    },
-    {
-        nome : 'marco',
-        cognome : 'giallini',
-        eta : '57'
-    }
-];
-// console.log(elencoStudenti);
-// stampo nome e cognome di tutti con ciclo
+    };
+
+    // lo scrivo nel dom
+    var datiStudente = '';
+    for (var key in studente) {
+        datiStudente += (" " + studente[key]);
+    };
+
+    $('#stud').append(datiStudente);
+
+    // creo array di oggetti
+    var elencoStudenti = [
+        {
+            nome : 'caio',
+            cognome : 'bianchi',
+            eta : '23'
+        },
+        {
+            nome : 'sempronio',
+            cognome : 'verdi',
+            eta : '25'
+        },
+        {
+            nome : 'marco',
+            cognome : 'giallini',
+            eta : '57'
+        }
+    ];
+
+    // stampo nome e cognome di tutti con ciclo
+    $('#studenti').html(stampa());
+
+    // aggiungo possibilità di inserire i prompt
+    var studenteInserito = {}
+    $('#bottone').click(function(){
+        var nome = prompt('inserisci il nome');
+        var cognome = prompt('inserisci il cognome');
+        var eta = prompt('inserisci l/età');
+        var studenteInserito = {
+            nome : nome,
+            cognome : cognome,
+            eta : eta
+        };
+        elencoStudenti.push(studenteInserito);
+        $('#studenti').html(stampa());
+        // console.log(elencoStudenti);
+    });
 
 
-for (var i = 0; i < elencoStudenti.length; i++) {
-    var nome = elencoStudenti[i].nome;
-    var cognome = elencoStudenti[i].cognome;
-    $('#studenti').append('nome: ' + nome + ', ' + 'cognome: ' + cognome + '<br>');
 
-};
+
+
+    // FUNZIONI
+    function stampa(){
+        var testo = '';
+        for (var i = 0; i < elencoStudenti.length; i++) {
+            var nome = elencoStudenti[i].nome;
+            var cognome = elencoStudenti[i].cognome;
+            testo += ('nome: ' + nome + ', ' + 'cognome: ' + cognome + '<br>')
+        };
+        return testo;
+    };
+
+});
